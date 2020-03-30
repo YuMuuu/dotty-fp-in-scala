057a675f6e4920bc3360ff97357e9ba551ed0ee4
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-03-31 00:26:20 +0900

    add tailrec fib

---
+++ src/main/scala/ch02/FibonacciNumber.scala
@@ -0,0 +1,10 @@
+package ch02
+import scala.annotation.tailrec
+
+object FibonacciNumber
+def fib(n: Int): Int =
+  @tailrec def loop(n: (Int, Int), max: Int): Int =
+    val (x, y) = n
+    if (max <= 0 ) x
+    else loop((y, x+y), max - 1)
+  loop((0, 1), n)
