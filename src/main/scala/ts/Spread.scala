8d681a8ab04bab4a8a6f1c667aea741cc14f6b8b
yu miura <curelemonade15@gmail.com>
2021-03-08 17:40:56 +0900

    add spread function

--- src/main/scala/ts/Spread.scala
+++ src/main/scala/ts/Spread.scala
@@ -1,16 +1,15 @@
 
 package ts
 
+// type maybeNestedList[T] = List[T | List[T]]
 type maybeNestedList[T] = List[T | List[T]]
+
 extension listOps on [T](nt: maybeNestedList[T]) {
   def spread: List[T] = nt flatten {
-      case i: List[T] => spread 
+      case i: List[T] => i.spread
       case e: T => List(e)
     }
 }
-
-val l: List[Int] = (List(1, 2, List(3, 4)): maybeNestedList[Int]).spread
-
 
 
 
--- src/test/scala/ts/SpreadSpec.scala
+++ src/test/scala/ts/SpreadSpec.scala
@@ -8,6 +8,8 @@ import scala.language.implicitConversions
 class SpreadSpec extends AnyFlatSpec with Diagrams {
   it should "nestの深さが違うlistをfllatenできる" in {
     assert((List(1, 2, List(3, 4)): maybeNestedList[Int]).spread === List(1, 2, 3, 4))
+    //再帰的定義はできていないのでこれは通らない
+    // assert((List(1, 2, List(3, 4), List(5, List(6, 7))): maybeNestedList[Int]).spread === List(1, 2, 3, 4)) 
   }
 }
 
